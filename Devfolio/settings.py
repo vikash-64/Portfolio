1c8d1929d42e3b4c584a8f7eb42aae3d7845ef46
Vikash Kr Upadhyay <vikash400001@gmail.com>
2022-03-29 18:50:13 +0530

    first commit

--- Devfolio/settings.py
+++ Devfolio/settings.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 import django_heroku 
 import dj_database_url
-from decouple import config 
+from decouple import config , os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
